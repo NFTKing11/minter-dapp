bbe553b0c62822201d336a3b2e35ab157bf6617e
Your Name <you@example.com>
2022-03-24 15:24:43 +0100

    updated style

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,9 +1,9 @@
 const contractAddress = "0xc1a6211F3544A2aAA79D605C3257258DD17A82B9"; //Replace with your own contract address
 const chain = 'rinkeby'; // rinkeby or polygon
 
-const welcome_h1 = "Welcome to the CodeCats NFT Project!!";
+const welcome_h1 = "Welcome to the Arnb-Bunny-Community NFT Project!!";
 const welcome_h2 = "Connect to MetaMask to Get Started";
-const welcome_p = 'The CodeCats NFT Project is a decentralized, open-source project that aims to demonstrate how to develope and launch your own NFT Collection. Follow step by step on the <a href="https://youtube.com/codestackr"  target="_blank" rel="noopener noreferrer">codeSTACKr</a> YouTube channel.';
+const welcome_p = 'The Arnb-Bunny-Community NFT Project is a Project that has benn developed by two siblings aged 15 and 11 ';
 const h1_presale_coming_soon = "NFT Drop Coming Soon!!";
 const h1_presale_mint = "Pre-Sale Minting Open!!";
 const h1_public_mint = "Public Minting Open!!";
